3b0257008760dfcc9e038a845a467db18908b302
Dawnabelle <dawn.mott@gmail.com>
2018-07-22 16:44:38 -0700

    add content to about and contacts

---
+++ src/pages/contact.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const FourthPage = () => (
+<div>
+  <div>
+    <h1>Contact Moi</h1>
+    <p><em>Contactez moi</em>, s'il vous plaît...</p>
+  </div>
+    <div>
+      <img src='https://dawnabelle.github.io/portfolio/img/idaho.jpg'/>
+      <p>... if you &hearts; hiking.</p>
+      <p>... if you &hearts; puppies.</p>
+      <p>... if you &hearts; old dogs</p>
+      <p>... if you &hearts; sci-fi.</p>
+      <p>... if you &hearts; fantasy.</p>
+      <p>... if you &hearts; code.</p>
+      <p>... if you &hearts; being nice.</p>
+      <p>... if you &hearts; green.</p>
+      <p>... if you &hearts; PoC.</p>
+      <p>... if you &hearts; freedom.</p>
+      <p>... if you &hearts; LGBTQ+.</p>
+      <img src='https://dawnabelle.github.io/portfolio/img/tardis.jpg'/>
+    </div>
+  </div>
+)
+
+export default FourthPage
---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const ThirdPage = () => (
+<div>
+  <p>Welcome to the eventual home of the Projects page.</p>
+  <img src='https://images.unsplash.com/photo-1531299411897-f9adcfb1a333?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5d4c981002e3298ac736245e85414c36&auto=format&fit=crop&w=934&q=80' alt='pretty, curly lady- turquoise top, orange skirt' width='500' />
+  <br />
+</div>
+)
+
+export default ThirdPage
