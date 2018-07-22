415c9e4e9d82fe15065816a0e053c2b2c1ccdcc2
Dawnabelle <dawn.mott@gmail.com>
2018-07-22 16:49:29 -0700

    images add to home

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,26 @@ import Link from "gatsby-link";
 
 export default () =>
   <div style={{
-      color: 'teal'
+      color: 'teal',
     }}>
     <h1>Dawnabelle in Real Life</h1>
     <p>Welcome.</p>
     <p>Please enjoy your pinhole glimpse of my life and code.</p>
     <p>Allons-y!</p>
+    <img src='https://dawnabelle.github.io/portfolio/img/beach.jpg' alt='' width='500' />
+    <img src='https://dawnabelle.github.io/portfolio/img/family.jpg' alt='' width='500' />
+    <img src='https://dawnabelle.github.io/portfolio/img/helens.jpg' alt='' width='500' />
+    <img src='https://dawnabelle.github.io/portfolio/img/bike.jpg' alt='' width='500' />
+    <img src='https://dawnabelle.github.io/portfolio/img/cuz.jpg' alt='' width='500' />
+    <img src='https://dawnabelle.github.io/portfolio/img/climb.jpg' alt='' width='500' />
+    <img src='https://dawnabelle.github.io/portfolio/img/kerbear.jpg' alt='' width='500' />
+    <img src='https://dawnabelle.github.io/portfolio/img/mountain.jpg' alt='' width='500' />
+    <img src='https://dawnabelle.github.io/portfolio/img/petunia.jpg' alt='' width='500' />
+    <img src='https://dawnabelle.github.io/portfolio/img/beach.jpg' alt='' width='500' />
+    <img src='https://dawnabelle.github.io/portfolio/img/family.jpg' alt='' width='500' />
+    <img src='https://dawnabelle.github.io/portfolio/img/helens.jpg' alt='' width='500' />
+    <img src='https://dawnabelle.github.io/portfolio/img/bike.jpg' alt='' width='500' />
+    <img src='https://dawnabelle.github.io/portfolio/img/cuz.jpg' alt='' width='500' />
     <img src='https://images.unsplash.com/photo-1504105547124-fc1aee34470d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5776563a3b4df93f2729a29e9bf5c27b&auto=format&fit=crop&w=1352&q=80' alt='retro room' width='500' />
     <br />
 
