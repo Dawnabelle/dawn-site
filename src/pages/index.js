f97f7402083a3a42507faa7ada2dc02d1326c340
Dawnabelle <dawn.mott@gmail.com>
2018-07-22 09:15:59 -0700

    build skeleton of pages and links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import Link from "gatsby-link";
 
-const IndexPage = () => (
-  <div style={{ color: `tomato` }}>
+export default () =>
+  <div style={{
+      color: 'teal'
+    }}>
     <h1>Hello Gatsby!</h1>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>Will this be fun to use to build my Portfolio for my Capstone project?</p>
+    <p>Results in a bit</p>
+    <img src='https://images.unsplash.com/photo-1504105547124-fc1aee34470d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5776563a3b4df93f2729a29e9bf5c27b&auto=format&fit=crop&w=1352&q=80' alt='retro room' width='500' />
+    <br />
+    <div>
+      <Link to='/page-2/'>Visit Page 2</Link>
+      <br/>
+      <Link to='/page-3/'>Vist Page 3</Link>
+    </div>
   </div>
-)
-
-export default IndexPage
