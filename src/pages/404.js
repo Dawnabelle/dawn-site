055ff96fe36963aff1a78984ccce6d5482dcff65
Dawnabelle <dawn.mott@gmail.com>
2018-07-22 16:33:40 -0700

    add content to about page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import cryingDonkey from '../assets/crying-donkey.gif';
+import stars from '../assets/stars.jpg';
 
-const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-)
+function Error404() {
+  return (
+    <div>
+      <style jsx="jsx">
+        {
+          ` .appStyle {
+            text-align: center;
+            margin: auto;
+            color: teal;
+            background-image: url(${stars});
+            height: 2000;
+          }
+          .img {
+            border-radius: 50%;
+            object-fit:cover;
+            width: 50%;
+            padding-top: 50;
+            opacity: .8;
+          }
+          .img:hover {
+            opacity: .5;
+          }
+           `
+        }
+      </style>
+      <div>
+        <img className="img" src={cryingDonkey}/>
+      </div>
+      <h2>You made the donkey cry.</h2>
+      <h3>Would you like to return <Link to="/"> home </Link> and leave them in peace?</h3>
+      <h3>Or would you like to <Link to="/drinks"> drink </Link> now?</h3>
+    </div>);
+}
 
-export default NotFoundPage
+Error404.propTypes = {
+  location: PropTypes.object
+};
+
+export default Error404;
