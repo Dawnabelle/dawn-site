0d17b2579ec9252e0ac986be5534a25d6407b81c
Dawnabelle <dawn.mott@gmail.com>
2018-07-23 15:55:21 -0700

    add layout overide styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: 'Dawn In Real Life',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-catch-links`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    }
+    `gatsby-transformer-remark`,
+  ],
 }
