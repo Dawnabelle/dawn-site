f351d4e67caf9981e6eb9501a9c18472a4485b40
Dawnabelle <dawn.mott@gmail.com>
2018-07-24 12:20:04 -0700

    format about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,55 +9,55 @@ const SecondPage = () => (
       padding: '20px 50px 20px 50px'
     }}>
     <div>
+      <h1>Dawnabelle in Real Life</h1>
         <div>
           <img src='https://dawnabelle.github.io/portfolio/img/egg.jpg' width='500'/>
           <div>
-          <h2>Thanks, Eglantine!</h2>
-          <h4>Substituiary Locomotion Spell</h4>
-          <p>Treguna Mekoides and Trecorum Satis Dee<br/>
-          Substitutiary locomotion mystic power<br/>
-          That's far beyond the wildest notion<br/>
-          It's so weird, so feared, yet wonderful to see<br/>
+          <h3>Substituiary Locomotion Spell</h3>
+            <p>Treguna Mekoides and Trecorum Satis Dee<br/>
+            Substitutiary locomotion mystic power<br/>
+            That's far beyond the wildest notion<br/>
+            It's so weird, so feared, yet wonderful to see<br/>
           Substitutiary locomotion come to me.<br/>
-        -Eglantine Price (fictional character)</p>
-          <h3>Sci-Fi &amp; Fantasy</h3>
-            <p>Doctor Who</p>
-            <p>The Magicians</p>
-            <p>Studio Ghibli</p>
-            </div>
+            -Eglantine Price (fictional character)</p><br/>
+          </div>
         </div>
-      </div>
+        <h3>Sci-Fi &amp; Fantasy Faves</h3>
+        <p>Doctor Who<br/>
+        The Magicians<br/>
+      Studio Ghibli</p>
+      </div><br/>
 
       <div>
         <div>
-          <h2>Mustard McGinks</h2>
+          <h2>Mustard McGinks<br/>
+          Oh, he stinks</h2>
           <p>When this photo was taken, he was nameless.</p>
           <img src='https://dawnabelle.github.io/portfolio/img/thumb.jpg' width='500'/>
-          <h3>Oh he stinks</h3>
           <h4>He's just a thumb</h4>
           <p>At the height of my depression, <br/>I found the most amazing puppy on the planet.<br/> He saved my life and made me a better person.<br/> I hope he understands what he means to me.</p>
           <div>
-            <h3>My Mustard Guy</h3>
-              <p>He's got soft ears.</p>
-              <p>He sleeps in my bed.</p>
-              <p>He'd follow me anywhere.</p>
+            <p>He's got soft ears.<br/>
+            He sleeps in my bed.<br/>
+            He'd follow me anywhere.<br/>
+          <strong>He's a Mustard Guy</strong></p>
           </div>
         </div>
       </div>
 
       <div>
+        <h2>The Great Outdoors</h2>
         <div>
           <img src='https://dawnabelle.github.io/portfolio/img/mustard.jpg' width='500'/>
-          <h2>The Great Outdoors</h2>
           <h3>Fresh air, free mind.</h3>
           <p>I've always loved being outside! In Florida, the beach was my escape. <br/>In NYC, it was Prospect Park. In Atlanta, it was Piedmont Park. <br/>Now that I'm in Oregon, I explore the Pacific Northwest as often as I possibly can. <br/>Always, with Mustard.</p>
           <div>
             <h3>Favorite Hiking Spots</h3>
-              <p>Columbia River Gorge</p>
-              <p>Olympic National Park</p>
-              <p>Oregon Coast</p>
-              <p>Silver Falls</p>
-              <p>Mt. Hood</p>
+              <p>Columbia River Gorge<br/>
+              Olympic National Park<br/>
+              Oregon Coast<br/>
+              Silver Falls<br/>
+            Mt. Hood</p>
           </div>
         </div>
       </div>
